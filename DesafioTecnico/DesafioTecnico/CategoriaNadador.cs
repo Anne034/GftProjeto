5539c0d36dafc5f04d34ba53ade59bb4bf427ab3
Edyane Araujo <annearaujo034@gmail.com>
2022-06-21 17:28:50 -0300

    Teste adc segundo exercicio

---
+++ DesafioTecnico/DesafioTecnico/CategoriaNadador.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioTecnico
+{
+    public class CategoriaNadador
+    {
+        public void RetornarCategoria()
+        {
+            Console.WriteLine("Exercicio categorias de Nadador");
+            Console.WriteLine("Digite uma idade:");
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Adulto");
+            }
+            else if (idade >= 14)
+            {
+                Console.WriteLine("Juvenil B");
+            }
+            else if (idade >= 11)
+            {
+                Console.WriteLine("Juvenil A");
+            }
+            else if (idade >= 8)
+            {
+                Console.WriteLine("Infantil B");
+            }
+            else
+            {
+                Console.WriteLine("Infantil A");
+            }
+        }
+    }
+}
--- DesafioTecnico/DesafioTecnico/Program.cs
+++ DesafioTecnico/DesafioTecnico/Program.cs
@@ -8,10 +8,24 @@ namespace DesafioTecnico
         {
             // Programa Censo 
 
-            Censo censo = new Censo();
-            censo.Ativar();
-            
-            
+            //Censo censo = new Censo();
+            //censo.Ativar();
+            //static void Main(string[] args)
+            {
+                //Censo censo = new Censo();
+                //censo.Ativar();
+
+                //Antigo teste....
+                CategoriaNadador cat = new CategoriaNadador();
+                Console.WriteLine(cat.RetornarCategoria(5));
+                Console.WriteLine(cat.RetornarCategoria(9));
+                Console.WriteLine(cat.RetornarCategoria(12));
+                Console.WriteLine(cat.RetornarCategoria(15));
+                Console.WriteLine(cat.RetornarCategoria(18));
+                Console.WriteLine(cat.RetornarCategoria(25));
+            }
+
+
         }
     }
 }
